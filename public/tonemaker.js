9f0135e1003815a825a0fecdfb5b02fc98dccf47
Ameco <hamecow@gmail.com>
2020-11-04 23:21:25 -0800

    Create tonemaker

---
+++ public/tonemaker.js
@@ -0,0 +1,9 @@
+let toneButton = document.querySelector(".soundbtn");
+//create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+toneButton.onclick = function() {
+    synth.triggerAttackRelease("C4", "8n");
+    console.log("clicked");
+}
