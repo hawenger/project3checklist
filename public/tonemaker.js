fbd7796df049e6e9874a30ef4fb2aeac8cd6addb
Ameco <hamecow@gmail.com>
2020-11-05 17:57:22 -0800

    Add cookies

--- public/tonemaker.js
+++ public/tonemaker.js
@@ -6,4 +6,33 @@ const synth = new Tone.Synth().toDestination();
 toneButton.onclick = function() {
     synth.triggerAttackRelease("C4", "8n");
     console.log("clicked");
+    
+    writeCookie();
+    readCookie();
+    //console.log(name);
+  
+    
+}
+
+function writeCookie() {
+    document.cookie="id = 3214;returning = true;"
+}
+
+function readCookie() {
+let currentCookie = document.cookie;
+if(currentCookie == "") {
+    console.log("hello")
+} else {
+    console.log("welcome back")
+}
+}
+
+function userName() {
+    name = prompt("What's a fun name I can call you by?")
+    if (name === oldName) {
+
+    }
+    if (name != null) {
+        
+    }
 }
--- server.js
+++ server.js
@@ -4,19 +4,21 @@ const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-//require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "./index.html"));
+    
   });
+  
 
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
+    
   });
   
