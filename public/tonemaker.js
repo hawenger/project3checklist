2390e65bd4d5318f20e7e34784f0ea619a8e8a5e
Ameco <hamecow@gmail.com>
2020-11-05 19:21:30 -0800

    Add node server

--- public/tonemaker.js
+++ public/tonemaker.js
@@ -1,13 +1,19 @@
 let toneButton = document.querySelector(".soundbtn");
 //create a synth and connect it to the main output (your speakers)
 const synth = new Tone.Synth().toDestination();
-const now = Tone.now();
+setInterval(() => console.log(Tone.now()), 100);
 
 //play a middle 'C' for the duration of an 8th note
-toneButton.onclick = function() {
-    synth.triggerAttackRelease("C4", "8n");
-    synth.triggerAttack("C4", now);
-    //console.log(name);
+//toneButton.onclick = function() {
+    
+    
+
   
     
-}
+//}
+
+document.querySelector('.soundbtn')?.addEventListener('click', async () => {
+	await Tone.start()
+    console.log('audio is ready')
+    synth.triggerAttackRelease("C4", "8n");
+});
